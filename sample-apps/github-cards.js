3a6109f5d87ee80a763f026053b935020e3b29ee
Marvin Garcia <magar@microsoft.com>
2024-01-12 13:52:43 -0500

    added sample React apps

---
+++ sample-apps/github-cards.js
@@ -0,0 +1,94 @@
+const testData = [
+    {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
+    {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
+    {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
+];
+
+const CardList = (props) => (
+	<div>
+  	{props.profiles.map(profile => <Card key={profile.id} {...profile}/>)}
+	</div>
+);
+
+class Card extends React.Component {
+	render() {
+  	const profile = this.props;
+  	return (
+    	<div className="github-profile">
+    	  <img src={profile.avatar_url} />
+        <div className="info">
+          <div className="name">{profile.name}</div>
+          <div className="company">{profile.company}</div>
+        </div>
+    	</div>
+    );
+  }
+}
+
+class Form extends React.Component {
+  state = { 
+    userName: ''
+  };
+  
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    // console.log(this.state.userName);
+    const response = await axios.get(`https://api.github.com/users/${this.state.userName}`);
+    this.props.onSubmit(response.data);
+    this.setState({ userName: '' });
+  };
+  
+  logEvent = (event) => {
+    event.preventDefault();
+    this.setState({ userName: event.target.value });
+    // console.log(event.target.value);
+  }
+  
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input 
+          type="text" 
+          placeholder="Github username" 
+          value={this.state.userName} 
+           // onChange={event => this.setState({ userName: event.target.value })} 
+          onChange={this.logEvent}
+          required
+        />
+        <button>Add card</button>
+      </form>
+    )
+  }
+}
+
+class App extends React.Component {
+  state = {
+    profiles: testData,
+  };
+  
+  addNewProfile = (profileData) => {
+    this.setState(prevState => ({
+      profiles: [...prevState.profiles, profileData],
+    }));
+  };
+  
+	render() {
+  	return (
+    	<div>
+    	  <div className="header">{this.props.title}</div>
+        <Form onSubmit={this.addNewProfile} />
+        <CardList profiles={this.state.profiles} />
+    	</div>
+    );
+  }	
+}
+
+ReactDOM.render(
+	<App title="The GitHub Cards App" />,
+  mountNode,
+);
+
+// *** The React 18 way:
+// root.render(
+//   <App title="The GitHub Cards App" />,
+// );
