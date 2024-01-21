95649162f1cbd923a567959594bf996be12c5a76
Marvin Garcia <magar@microsoft.com>
2024-01-21 18:40:34 -0500

    Added stations pie chart

--- FuelPriceOptimizer/gaspricecalculator.client/src/components/Stations.js
+++ FuelPriceOptimizer/gaspricecalculator.client/src/components/Stations.js
@@ -6,6 +6,7 @@ import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
 import { APIProvider, Map, Marker, useMapsLibrary } from "@vis.gl/react-google-maps";
+import Chart from "react-google-charts";
 
 function StationsGrid({ stations, onRowSelectionChanged }) {
   const gridRef = useRef();
@@ -56,35 +57,30 @@ function StationsGrid({ stations, onRowSelectionChanged }) {
       field: 'cot',
       filter: 'agTextColumnFilter',
     },
-    {
-      field: 'latitude',
-      filter: 'agNumberColumnFilter',
-    },
-    {
-      field: 'longitude',
-      filter: 'agNumberColumnFilter',
-    }
   ]), []);
 
   return (
     <div className="row">
       <div className="col-lg-12">
         <div className="card">
-          <div className="card-body">
-            <h5 className="card-title">Stations</h5>
-            <div className="ag-theme-alpine" style={gridStyle}>
-              <AgGridReact
-                ref={gridRef}
-                rowData={stations}
-                columnDefs={columnDefs}
-                defaultColDef={defaultColDef}
-                pagination={true}
-                paginationPageSizeSelector={paginationPageSizeSelectors}
-                paginationPageSize={paginationPageSize}
-                rowSelection={'multiple'}
-                onSelectionChanged={() => onRowSelectionChanged(gridRef.current)}
-              />
-            </div>
+          <div className="card-body d-flex align-items-center">
+            <h5 className="card-title mb-0">Stations</h5>
+            {/* <button type="reset" className="btn btn-secondary ml-auto">
+              Reset
+            </button> */}
+          </div>
+          <div className="ag-theme-alpine" style={gridStyle}>
+            <AgGridReact
+              ref={gridRef}
+              rowData={stations}
+              columnDefs={columnDefs}
+              defaultColDef={defaultColDef}
+              pagination={true}
+              paginationPageSizeSelector={paginationPageSizeSelectors}
+              paginationPageSize={paginationPageSize}
+              rowSelection={'multiple'}
+              onSelectionChanged={() => onRowSelectionChanged(gridRef.current)}
+            />
           </div>
         </div>
       </div>
@@ -95,7 +91,7 @@ function StationsGrid({ stations, onRowSelectionChanged }) {
 function StationsMap(props) {
   const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
   const mapContainerStyle = {
-    height: 400,
+    height: "400px",
     width: "100%",
   };
   const center = {
@@ -118,9 +114,6 @@ function StationsMap(props) {
       center['lat'] = props.mapBounds.north - ((props.mapBounds.north - props.mapBounds.south) / 2);
       center['lng'] = props.mapBounds.east - ((props.mapBounds.east - props.mapBounds.west) / 2);
     }
-
-    console.log(props.mapBounds);
-    console.log(useBounds);
   }
 
   return (
@@ -151,11 +144,35 @@ function StationsMap(props) {
   );
 }
 
+function StationsPieChart(props) {
+  const chartContainerStyle = {
+    height: "400px",
+    width: "100%",
+  };
+
+  const options = {
+    title: 'Stations per Zone Id',
+    pieHole: 0.4,
+    is3D: false,
+  };
+
+  return (
+    <Chart
+      chartType="PieChart"
+      data={props.data}
+      options={options}
+      height={chartContainerStyle.height}
+      width={chartContainerStyle.width}
+    />
+  );
+};
+
 const StationsView = ({ backend_url }) => {
   const [stations, setStations] = useState([]);
   const [selectedStations, setSelectedStations] = useState([]);
   const [mapId, setMapId] = useState(1);
   const [mapBounds, setMapBounds] = useState(null);
+  const [chartData, setChartData] = useState([]);
 
   const onStationSelectionChanged = (grid) => {
     // create new map ID to be used as a key
@@ -188,6 +205,22 @@ const StationsView = ({ backend_url }) => {
   }, [backend_url]);
 
   useEffect(() => {
+    const countPerZoneId = stations => {
+      let stationCountArray = [['Task', 'Stations per Zone']];
+      let zoneIdArray = [...new Set(stations.map(station => station.zoneId))];
+      for (let i = 0; i < zoneIdArray.length; i++) {
+        let zoneId = zoneIdArray[i];
+        let count = stations.filter(station => station.zoneId === zoneId).length;
+        stationCountArray.push([zoneId, stations.filter(station => station.zoneId === zoneId).length]);
+      }
+
+      setChartData(stationCountArray);
+    };
+
+    countPerZoneId(stations);
+  }, [stations]);
+
+  useEffect(() => {
     const calculateMapBounds = () => {
 
       if (selectedStations.length > 0) {
@@ -215,7 +248,12 @@ const StationsView = ({ backend_url }) => {
   return (
     <section className="section">
       <StationsGrid stations={stations} onRowSelectionChanged={onStationSelectionChanged} />
-      <StationsMap stations={selectedStations} mapId={mapId} mapBounds={mapBounds} />
+      <div>
+        <div className="container-fluid">
+          <StationsMap stations={selectedStations} mapId={mapId} mapBounds={mapBounds} />
+          <StationsPieChart data={chartData} />
+        </div>
+      </div>
     </section>
   );
 };
