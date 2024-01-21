e9e6e55a0dad7a95130443f6c8b669a60e903ab4
Marvin Garcia <magar@microsoft.com>
2024-01-21 10:43:41 -0500

    updated stations grid

--- FuelPriceOptimizer/gaspricecalculator.client/src/components/Stations.js
+++ FuelPriceOptimizer/gaspricecalculator.client/src/components/Stations.js
@@ -5,45 +5,82 @@ import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
 
 function StationsTable({ backend_url }) {
+  const cont4ainerStyle = useMemo(() => ({ width: '100%', height: '100%' }), []);
+  const gridStyle = useMemo(() => ({ height: 800, width: '100%' }), []);
+  const paginationPageSizeSelectors = useMemo(() => ([30, 50, 100]), []);
+  const paginationPageSize = useMemo(() => (50), []);
   const [rowData, setRowData] = useState([]);
-  const [columnDefs, setColumnDefs] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
 
   const defaultColDef = useMemo(() => ({
     sortable: true,
     filter: true,
+    resizable: true,
+    flex: 1,
+    filterParams: {
+      buttons: ['reset']
+    },
+    floatingFilter: true,
   }), []);
+
+  const columnDefs = useMemo(() => ([
+    {
+      field: 'zoneId',
+      filter: 'agTextColumnFilter',
+    },
+    {
+      field: 'stationNumber',
+      filter: 'agNumberColumnFilter',
+    },
+    {
+      field: 'stationName',
+      filter: 'agTextColumnFilter',
+    },
+    {
+      field: 'streetAddress',
+      filter: 'agTextColumnFilter',
+    },
+    {
+      field: 'city',
+      filter: 'agTextColumnFilter',
+    },
+    {
+      field: 'state',
+      filter: 'agTextColumnFilter',
+    },
+    {
+      field: 'zipCode',
+      filter: 'agNumberColumnFilter',
+    },
+    {
+      field: 'cot',
+      filter: 'agTextColumnFilter',
+    }
+  ]), []);
 
   const tableRef = useRef();
 
   useEffect(() => {
-    const fetchData = async () => {
+    const getStationsData = async () => {
       try {
-        const response = await axios.get(`${backend_url}/stations`);
-        const data = response.data;
-        if (Array.isArray(data)) {
-          let _columnDefs = [];
-          for (const key in data[0]) {
-            _columnDefs.push({
-              field: key,
-              sortable: true,
-              filter: true,
-            });
-          }
+        console.log('getting stations data');
+        // let response = await axios.get(`${backend_url}/stations/summary`);
+        // let data = response.data;
+        // setStationsSummary(data);
 
+        const response = await axios.get(`${backend_url}/stations`);
+        const  data = response.data;
+        if (Array.isArray(data)) {
+          console.log('data rows:', data.length);
+          console.log('data sample:', data[0]);
           setRowData(data);
-          setColumnDefs(_columnDefs);
-          console.log('rows', rowData);
-          console.log('cols:', columnDefs);
         }
       } catch (error) {
-        setError(error.message);
-        setLoading(false);
+        console.log(error.message);
       }
     };
 
-    fetchData();
+    getStationsData();
+
   }, []);
 
   return (
@@ -52,12 +89,15 @@ function StationsTable({ backend_url }) {
         <div className="card">
           <div className="card-body">
             <h5 className="card-title">Stations</h5>
-            <div className="ag-theme-alpine" style={{ height: 800 }}>
+            <div className="ag-theme-alpine" style={gridStyle}>
               <AgGridReact
                 ref={tableRef}
                 rowData={rowData}
                 columnDefs={columnDefs}
                 defaultColDef={defaultColDef}
+                pagination={true}
+                paginationPageSizeSelector={paginationPageSizeSelectors}
+                paginationPageSize={paginationPageSize}
               />
             </div>
           </div>
