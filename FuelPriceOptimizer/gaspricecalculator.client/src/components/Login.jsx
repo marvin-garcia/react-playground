2dd2ad3b1f1cfe3c12cea880864246392a2870bb
Marvin Garcia <magar@microsoft.com>
2024-01-30 10:49:41 -0500

    tried to improve login page style

--- FuelPriceOptimizer/gaspricecalculator.client/src/components/Login.jsx
+++ FuelPriceOptimizer/gaspricecalculator.client/src/components/Login.jsx
@@ -15,31 +15,40 @@ const Login = () => {
   };
 
   return (
-    <div class="container">
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
-            <div class="d-flex justify-content-center py-4">
-              <a href="/" class="logo d-flex align-items-center w-auto">
-                <img src="assets/img/logo.png" alt="" />
-                <span class="d-none d-lg-block">FuelPriceOptimizer</span>
-              </a>
-            </div>
-            <div class="card mb-3">
-              <div class="card-body">
-                <div class="pt-4 pb-2">
-                  <h5 class="card-title text-center pb-0 fs-4">Sign-in</h5>
-                </div>
-                <div class="col-12">
-                  <button class="btn btn-primary w-100" type="button" onClick={handleLoginRedirect}>Sign in</button>
+    <div style={{width: "100%", boxSizing: "border-box"}}>
+      <section className="section register d-flex align-items-center justify-content-center py-4" style={{width: "100%"}}>
+        <div className="container" style={{width: "100%"}}>
+          <div className="row justify-content-center">
+            <div className="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
+              <div className="d-flex justify-content-center py-4">
+                <a href="index.html" className="logo d-flex align-items-center w-auto">
+                  <img src="assets/img/logo.png" alt="" />
+                  <span className="d-none d-lg-block">FuelPriceOptimizer</span>
+                </a>
+              </div>
+              <div className="card mb-3">
+                <div className="card-body">
+                  <div className="pt-4 pb-2">
+                    <h5 className="card-title text-center pb-0 fs-4">Login to Your Account</h5>
+                  </div>
+                  <div className="container">
+                    <button
+                      className="btn btn-primary w-100"
+                      type="button"
+                      style={{ marginTop: "40px" }}
+                      onClick={handleLoginRedirect}
+                    >
+                      Sign in
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </section>
     </div>
   );
-}
+};
 
 export default Login;
